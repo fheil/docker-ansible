3e5fcead301df1d9e2d9631e03bc817a3fb71945
Frank H. Heil <fhheil@gmail.com>
2024-05-01 14:52:40 +0200

    Update profile.local.sh

--- ansible/config/profile.local.sh
+++ ansible/config/profile.local.sh
@@ -1,6 +1,7 @@
-PROMPT_COMMAND='PS1=\`if test \"\$UID\" = 0 ; then
-        echo \"\033[37;43;1;1m[\h]\033[0m\w $ \" ;
-    else
-        echo \"\033[34;47;1;7m[\u@\h]\033[0m\w > \" ;
-    fi \`'
+PROMPT_COMMAND='PS1=\
+ `if test "$UID" = 0 ; then \
+        echo "\033[37;43;1;1m[\h]\033[0m\w # " ; \
+  else \
+        echo "\033[34;47;1;7m[\u@\h]\033[0m\w > " ; \
+  fi `'
 export PROMPT_COMMAND
